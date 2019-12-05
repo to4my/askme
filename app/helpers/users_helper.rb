ff47246feb5d385424710e16c20c2d108a7fd272
to4my <to4my@ya.ru>
2019-12-05 15:53:43 +0300

    add coun question

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+  def sklonenie(number, one, few, many, with_number = false)
+    number = 0 if number.nil? || !number.is_a?(Numeric)
+    prefix = with_number ? "#{number} " : ''
+
+    return prefix + many if (number % 100).between?(11, 14)
+
+    word = case number % 10
+           when 1 then one
+           when 2..4 then few
+           else
+             many
+           end
+
+    prefix + word
+  end
+
+  def questions_count(questions)
+    questions.count
+  end
 end
