0c76762f84d495167273d5ce3055d2fe3c676fcc
MikeP <to4my@ya.ru>
2019-12-09 23:22:31 +0300

    auth user after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,15 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      # логиним пользователя сразу
+      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
     else
       render 'new'
     end
+
+    user = User.authenticate(params[:email], params[:password])
+
   end
 
   def edit
